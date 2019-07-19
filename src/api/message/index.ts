618c17633181141e64a0ab2d661e6261b393af9c
Caian R. Ertl <hi@caian.org>
2019-07-19 03:22:06 -0300

    feat: if not me, ignore; if it ts, send the command to the broker

--- src/api/message/index.ts
+++ src/api/message/index.ts
@@ -1,27 +1,31 @@
+import { Broker } from '../../lib/broker';
 import { TelegramClient } from '../../lib/telegram';
+
 import { NowRequest, NowResponse } from '@now/node';
 
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 export default async function(req: NowRequest, res: NowResponse): Promise<void>
 {
-    const token: string = process.env.TELEGRAM_TOKEN || '';
-    const client = new TelegramClient(token);
+    const message = req.body.message;
 
+    const dialog: string  = message.text;
+    const msgId: number   = message.message_id;
+    const chatId: number  = message.chat.id;
     const ownerId: number = Number(process.env.TELEGRAM_OWNER_ID) || 0;
 
-    const event = req.body;
-    const message = event.message;
-
-    const msgId: number = message.message_id;
-    const chatId: number = message.chat.id;
-
-    if (chatId === ownerId) {
+    if (chatId !== ownerId) {
         const imageUrl = 'https://glitchy.s3.amazonaws.com/nope.gif';
+
+        const token  = process.env.TELEGRAM_TOKEN || '';
+        const client = new TelegramClient(token);
         await client.sendAnimation(chatId, imageUrl, msgId, '*You didn\'t say the magic word!*');
 
         res.end();
     }
+
+    const broker = new Broker(chatId, msgId, dialog);
+    await broker.emit();
 
     res.end();
 }
